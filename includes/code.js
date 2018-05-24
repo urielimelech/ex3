9502b5d045219fb013e126cf612ce49395d9cfc2
uri <urielimelech@se.shenkar.ac.il>
2018-05-24 18:28:53 +0300

    little updates
    deleted few lines which is not nececery.

--- includes/code.js
+++ includes/code.js
@@ -1,19 +1,14 @@
 var frame = document.getElementById("wrapper");
 var plus = document.getElementById("addCubes");
-j=0;
 window.onload=function(){
     for(i=0;i<4;i++){
         var cube = document.createElement("article");
         cube.style.opacity=Math.random();
         frame.appendChild(cube);
-        console.log('test');
     }
 }
 plus.onclick=function(){
-    if(j<8){
         var cube = document.createElement("article");
         cube.style.opacity=Math.random();
         frame.appendChild(cube);
-        j++;
-    }
 }

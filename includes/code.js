30753b15c5aed1dabc5cd84d4a017950212f96d9
uri <urielimelech@se.shenkar.ac.il>
2018-05-24 21:39:34 +0300

    7th commit
    shorting js code

--- includes/code.js
+++ includes/code.js
@@ -1,14 +1,12 @@
 var frame = document.getElementById("wrapper");
 var plus = document.getElementById("addCubes");
 window.onload=function(){
-    for(i=0;i<4;i++){
-        var cube = document.createElement("article");
-        cube.style.opacity=Math.random();
-        frame.appendChild(cube);
-    }
+    for(i=0;i<4;i++){createCubes()}
 }
-plus.onclick=function(){
-        var cube = document.createElement("article");
-        cube.style.opacity=Math.random();
-        frame.appendChild(cube);
+plus.onclick=function(){createCubes()}
+
+function createCubes(){
+    var cube = document.createElement("article");
+    cube.style.opacity=Math.random();
+    frame.appendChild(cube);
 }
